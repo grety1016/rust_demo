97dac4ad0a65d45f764951bc65ae8455b09a1d04
grety <snl1016@163.com>
2023-02-25 10:24:06 +0800

    commit

--- rust0/src/main.rs
+++ rust0/src/main.rs
@@ -1,4 +1,18 @@
+#[allow(dead_code)]
+#[allow(unused_imports)]
+
 use hello_world;
 fn main() {
-   hello_world::hello(); 
+   //hello_world::hello(); 
+
+   let hello = String::from("he llo");
+
+   let hello_bytes = hello.as_bytes();
+
+   for (i, &item) in hello_bytes.iter().enumerate(){
+      if item == b' '{
+         println!("{0},{1}",i,item);
+      }
+      
+   }
 }
